b0ab678dc529536cc94b21b04ec41ac732fe35ac
Arjun Nair <arjunnp8919@gmail.com>
2021-09-06 22:22:06 +0530

    Update AzureAD-ChangeDeviceOwner.ps1

--- AzureAD-ChangeDeviceOwner.ps1
+++ AzureAD-ChangeDeviceOwner.ps1
@@ -1,11 +1,8 @@
 ﻿param (
-
 [string]$DeviceName = (Read-Host -prompt 'Enter Device name'),
 [string]$CurrentOwner = (Read-Host -prompt 'Enter Current Owner of $DeviceName'),
 [string]$NewOwner = (Read-Host -prompt 'Enter New Owner of $DeviceName')
 )
-
-
 
 <#
 $Device_Object = 'LAPTOP-AzureAD1'
@@ -41,11 +38,11 @@ $NewOwnerName = $NewOwnerRefObjectId.DisplayName
 #getting device ownership
 $GetRegCurrentOwner = Get-AzureADDeviceRegisteredOwner -ObjectId $Device.ObjectId
 
-#add new owner to device Where,
-
-#-ObjectId is to specify the object id of the device
-
-#-RefObjectId is to specify the object ID of the user you want to add as registered owner.
+<#
+add new owner to device Where,
+-ObjectId is to specify the object id of the device
+-RefObjectId is to specify the object ID of the user you want to add as registered owner.
+#>
 
 If ($GetRegCurrentOwner.UserPrincipalName -eq $NewOwnerRefObjectId.UserPrincipalName){
 Write-Host "$NewOwnerName is already the the owner of $Device_Object. Not $CurrentOwnerName" -ForegroundColor red -BackgroundColor white
@@ -57,12 +54,13 @@ $AddnewOwner = Add-AzureADDeviceRegisteredOwner -ObjectId $Device.ObjectId -RefO
 }
 
 
+<#
+Remove Current owner from device Where,
 
-#Remove Current owner from device Where,
+-ObjectId is to specify the object id of the device
 
-#-ObjectId is to specify the object id of the device
-
-#-OwnerId is to specify the Current registered owner.
+-OwnerId is to specify the Current registered owner
+#>
 $Device = Get-AzureADDevice -SearchString $Device_Object
 $Owner = Get-AzureADDeviceRegisteredOwner -ObjectId $Device.ObjectId #| Where-Object {$_.UserPrincipalName -eq $CurrentRegOwner}
 If ($Owner.UserPrincipalName -match $CurrentRegOwner){
